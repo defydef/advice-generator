40a1bfc55eab83668b378e26cae3edce7a79a97c
defy <8841170+defydef@users.noreply.github.com>
2023-11-21 20:25:08 +0700

    Add box shadow on button hover

--- src/ui/Card.jsx
+++ src/ui/Card.jsx
@@ -28,8 +28,8 @@ function Card() {
         className="hidden sm:block max-w-[30vw]"
       />
 
-      <div className="grid grid-cols-[1fr_2rem_2rem_1fr] h-[2rem] w-[90vw]">
-        <div className="rounded-full w-[4rem] h-[4rem] bg-[var(--neon-green)] col-start-2 col-span-2 flex justify-center items-center">
+      <div className="grid grid-cols-[1fr_2rem_2rem_1fr] h-[2rem] w-[90vw] cursor-pointer ">
+        <div className="rounded-full w-[4rem] h-[4rem] bg-[var(--neon-green)] col-start-2 col-span-2 flex justify-center items-center hover:shadow-[0px_0px_40px_0px_var(--neon-green)]">
           <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
             <path
               d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z"
